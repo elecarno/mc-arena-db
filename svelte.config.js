7210fb6550187fe7fbd9fd036ed150309327f90d
bm_laptop <benrobmc.mc@gmail.com>
2024-06-18 12:53:04 +0100

    vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+		adapter: adapter({
+			// see below for options that can be set here
+		})
+	}
 }
